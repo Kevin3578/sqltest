895d92b09dccc6daadfbe352b230c443ddfa12bc
Kevin <kevin.yang@live.com>
2023-11-16 05:33:49 -0500

    test

--- models/orders.sql
+++ models/orders.sql
@@ -18,10 +18,11 @@ order_payments as (
         order_id,
 
         {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
+        sum(case when payment_method = '{{ payment_method }}'
+         then amount else 0 end) as {{ payment_method }}_amount,
         {% endfor -%}
 
-        sum(amount) as total_amount
+        sum(amount) as day
 
     from payments
 
